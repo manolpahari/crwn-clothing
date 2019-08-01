13166dc40ad4b40b1a7af92001ccab21b16e8df9
manolph <manol@webpoint.io>
2019-08-01 15:57:25 -0600

    update our menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import HomePage from '../src/pages/homepage-component';
 
 function App() {
+
+  const hatsPage = (props) => {
+    console.log(props);
+    return <h1>HATS PAGE</h1>
+  }
   return (
     <div className="App">
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop/hats' component={hatsPage}/>
     </div>
   );
 }
