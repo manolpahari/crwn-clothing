7fff86c82bb3f0ee9f5bb4d44e6d75b17a7cc8e9
manolph <manol@webpoint.io>
2019-08-01 15:21:33 -0600

    broken our homepage into smaller components, finished styling our menu-items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './menu-item.styles.scss'
 const MenuItem = ({ title, imageUrl, size }) => {
     return (
-        <div style={{backgroundImage:`URL(${imageUrl})`}}
-        className={`${size} menu-item`}>
+        <div className={`${size} menu-item`}>
+            <div className="background-image"
+            style={{backgroundImage:`URL(${imageUrl})`}}
+            />
             <div className="content">
-                <h1 className="title">{title}</h1>
+                <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
