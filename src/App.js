0c7de51bb027891d97753b750d73d158e60fec91
manolph <manol@webpoint.io>
2019-08-05 13:52:23 -0600

    adding header component and liks to home page, shop page and contact page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from '../src/pages/homepage-component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 
 function App() {
 
   return (
     <div className="App">
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop' component={ShopPage}/>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
